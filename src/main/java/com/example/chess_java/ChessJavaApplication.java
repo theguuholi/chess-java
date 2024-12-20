6a81cfb02abe1fc5d65a1f9d243a2ae1b33d1f05
Gus <g.92oliveira@gmail.com>
2024-12-20 13:04:03 -0300

    start

---
+++ src/main/java/com/example/chess_java/ChessJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.chess_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessJavaApplication.class, args);
+	}
+
+}
