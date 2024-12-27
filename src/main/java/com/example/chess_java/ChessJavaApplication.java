83cda9c62811ddcb0d6dae3834e0421aaff0bd8c
Gus <g.92oliveira@gmail.com>
2024-12-27 10:51:03 -0300

    printing possible moves

--- src/main/java/com/example/chess_java/ChessJavaApplication.java
+++ src/main/java/com/example/chess_java/ChessJavaApplication.java
@@ -19,6 +19,12 @@ public class ChessJavaApplication {
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(in);
+
+
+				boolean[][] possibleMovies = chessMatch.possibleMoves(source);
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces(), possibleMovies);
+				
 				System.out.println();
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(in);
--- src/main/java/com/example/chess_java/UI.java
+++ src/main/java/com/example/chess_java/UI.java
@@ -37,21 +37,36 @@ public class UI {
         for (int i = 0; i < pieces.length; i++) {
             System.out.print((8 - i) + " ");
             for (int j = 0; j < pieces.length; j++) {
-                printPiece(pieces[i][j]);
+                printPiece(pieces[i][j], false);
             }
             System.out.println();
         }
         System.out.println("  a b c d e f g h");
     }
 
-    private static void printPiece(ChessPiece chessPiece) {
+    public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
+        for (int i = 0; i < pieces.length; i++) {
+            System.out.print((8 - i) + " ");
+            for (int j = 0; j < pieces.length; j++) {
+                printPiece(pieces[i][j], possibleMoves[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println("  a b c d e f g h");
+    }
+
+    private static void printPiece(ChessPiece chessPiece, boolean background) {
         // if (chessPiece == null) {
         // System.out.print("-");
         // } else {
         // System.out.print(chessPiece);
         // }
+        if (background) {
+            System.out.print(ANSI_BLUE_BACKGROUND);
+        }
+
         if (chessPiece == null) {
-            System.out.print("-");
+            System.out.print("-" + ANSI_RESET);
         } else {
             if (chessPiece.getColor() == Color.WHITE) {
                 System.out.print(ANSI_WHITE + chessPiece + ANSI_RESET);
